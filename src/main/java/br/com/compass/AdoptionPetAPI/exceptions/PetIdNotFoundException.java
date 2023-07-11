45f14bbbbcc93da3aee6899087c8929fc851cc48
IanZeta64 <ianpf7@gmail.com>
2023-07-11 17:10:56 -0300

    Handler advice

--- src/main/java/br/com/compass/AdoptionPetAPI/exceptions/PetIdNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.compass.AdoptionPetAPI.exceptions;
-
-public class PetIdNotFoundException extends RuntimeException{
-  public PetIdNotFoundException(String message){
-    super(message);
-  }
-}
